7f78ce8c17b2c8b45444e1ac8d01e87435d08748
shaoshitong <1090784053@qq.com>
2024-08-08 08:38:36 +0000

    second commit

--- animatediff_noise_optimization_test.py
+++ animatediff_noise_optimization_test.py
@@ -20,21 +20,6 @@ pipe.enable_vae_slicing()
 pipe.enable_model_cpu_offload()
 
 
-# pipe.recall_timesteps = 0
-# pipe.ensemble = 1
-# pipe.pre_free_init_enabled = True
-# pipe.fast_ensemble = False
-# local_path = "./result_vanilla.gif"
-# output = pipe(
-#     prompt="Trump was shot in the left ear during the presidential campaign, yet he still pumped his fist up to lift the crowd",    # negative_prompt="",
-#     num_inference_steps=20,
-#     guidance_scale=7,
-#     width=1024,
-#     height=1024,
-#     num_frames=16,
-# )
-# frames = output.frames[0]
-# export_to_gif(frames, local_path)
 
 # pipe.recall_timesteps = 1
 # pipe.ensemble = 1
@@ -53,29 +38,40 @@ pipe.enable_model_cpu_offload()
 # export_to_gif(frames, local_path)
 
 
-pipe.recall_timesteps = 0
-pipe.ensemble = 1
+pipe.recall_timesteps = 1
+pipe.ensemble = 50
 pipe.pre_free_init_enabled = False
 pipe.fast_ensemble = False
+pipe.ensemble_rate = 0.025
+pipe.ensemble_guidance_scale = True
+# prompts = [
+#     "A young woman in a yellow sweater uses VR glasses, sitting on the shore of a pond on a background of dark waves. A strongwind develops her hair, the sun's rays are reflected from the water.",
+#     "A bustling futuristic cityscape at night with neon lights, flying cars, and towering skyscrapers.",
+#     "A serene countryside scene with rolling green hills, a small river, and a cozy cottage with smoke coming from the chimney.",
+#     "A majestic dragon flying over a medieval castle perched on a cliff, with the sun setting in the background.",
+#     "A beautiful underwater scene with colorful coral reefs, various types of fish, and a sunken pirate ship.",
+#     "A vibrant street market in a small village in India, with colorful stalls, people in traditional clothing, and spices in the air."
+# ]
 
 prompts = [
-    "A young woman in a yellow sweater uses VR glasses, sitting on the shore of a pond on a background of dark waves. A strongwind develops her hair, the sun's rays are reflected from the water.",
-    "A bustling futuristic cityscape at night with neon lights, flying cars, and towering skyscrapers.",
-    "A serene countryside scene with rolling green hills, a small river, and a cozy cottage with smoke coming from the chimney.",
-    "A majestic dragon flying over a medieval castle perched on a cliff, with the sun setting in the background.",
-    "A beautiful underwater scene with colorful coral reefs, various types of fish, and a sunken pirate ship.",
-    "A vibrant street market in a small village in India, with colorful stalls, people in traditional clothing, and spices in the air."
+    "Spiderman is surfing",
+    "Yellow and black tropical fish dart through the sea",
+    "An epic tornado attacking above aglowing city at night",
+    "Slow pan upward of blazing oak fire in an indoor fireplace",
+    "a cat wearing sunglasses and working as a lifeguard at pool",
+    "A dog in astronaut suit and sunglasses floating in space"
 ]
 
-local_path = "./result_fe.gif"
-output = pipe(
-    prompt="In slow motion, a dessert fork gently presses into the center of the cake. As the fork goes deeper, the outer crust begins to crack.",    # negative_prompt="",
-    num_inference_steps=50,
-    guidance_scale=7,
-    width=1024,
-    height=1024,
-    num_frames=16,
-)
-frames = output.frames[0]
-export_to_gif(frames, local_path)
+for i, prompt in enumerate(prompts):
+    local_path = f"./result_rgs_{i}.gif"
+    output = pipe(
+        prompt=prompt,    # negative_prompt="",
+        num_inference_steps=50,
+        guidance_scale=7,
+        width=512,
+        height=512,
+        num_frames=16,
+    )
+    frames = output.frames[0]
+    export_to_gif(frames, local_path)
 
