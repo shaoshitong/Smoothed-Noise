a8bb9236925f33d0f49cb61036de333ddf5114a0
shaoshitong <1090784053@qq.com>
2024-08-22 11:51:13 +0000

    update modelscope t2v

--- utils/pipeline_modelscope_t2v.py
+++ utils/pipeline_modelscope_t2v.py
@@ -31,6 +31,13 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
         )
         self.recall_timesteps = 1
         self.ensemble = 1
+        self.ensemble_rate = 0.1
+        self.pre_num_inference_steps = 50
+        self.fast_ensemble = False
+        self.momentum = 0.
+        self.traj_momentum = 0.05
+        self.ensemble_guidance_scale = False
+        self.noise_type = "uniform"
     
     @torch.no_grad()
     def __call__(self,
@@ -100,6 +107,12 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
         
         for i in range(_optim_steps):
             if self.ensemble == 1:
+                if self.ensemble_guidance_scale:
+                    guidance_scale = guidance_scale
+                    rand = torch.randn(1).item()
+                positive_guidance_scale = guidance_scale if not self.ensemble_guidance_scale else guidance_scale + rand
+                negative_guidance_scale = guidance_scale if not self.ensemble_guidance_scale else guidance_scale + rand
+            
                 _latent_model_input = torch.cat([_latents] * 2) if _do_classifier_free_guidance else _latents # Forward DDIM
                 _latent_model_input = _emergency_scheduler.scale_model_input(_latent_model_input, _lasting_t)
                 _noise_pred = self.unet(
@@ -111,7 +124,7 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                 )[0]
                 if _do_classifier_free_guidance:
                     _noise_pred_uncond, _noise_pred_text = _noise_pred.chunk(2)
-                    _noise_pred = _noise_pred_uncond + guidance_scale * (_noise_pred_text - _noise_pred_uncond)
+                    _noise_pred = _noise_pred_uncond + positive_guidance_scale * (_noise_pred_text - _noise_pred_uncond)
                 # Additional
                 _bsz, _channel, _frames, _width, _height = _latents.shape
                 _latents = _latents.permute(0, 2, 1, 3, 4).reshape(_bsz * _frames, _channel, _width, _height)
@@ -128,7 +141,7 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                 ).sample
                 if _do_classifier_free_guidance:
                     _noise_pred_uncond, _noise_pred_text = _noise_pred.chunk(2)
-                    _noise_pred = _noise_pred_uncond + guidance_scale * (_noise_pred_text - _noise_pred_uncond)
+                    _noise_pred = _noise_pred_uncond + negative_guidance_scale * (_noise_pred_text - _noise_pred_uncond)
                 _inv_extra_step_kwargs = copy.deepcopy(extra_step_kwargs)
                 _inv_extra_step_kwargs.pop("eta")
                 # Additional
@@ -138,8 +151,31 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                 _latents = _latents[None, :].reshape(_bsz, _frames, _channel, _width, _height).permute(0, 2, 1, 3, 4)
             else:
                 results = []
+                _prev_latents = _latents.clone()
+                _prev_prev_latents = _latents.clone()
+                
                 for j in range(self.ensemble):
-                    _latents_n = torch.randn_like(_latents) * 0.1 + _latents
+                    if self.noise_type == "uniform":
+                        print("pass uniform")
+                        additional_noise = (torch.rand_like(_latents) - 0.5) * 2 * (3 ** (1/2))
+                    elif self.noise_type == "truncated_gaussian":
+                        additional_noise = torch.randn_like(_latents).clip_(-1, 1)
+                    else:
+                        additional_noise = torch.randn_like(_latents)
+                    if self.fast_ensemble:
+                        print("pass fast-ensemble")
+                        _prev_prev_latents = _prev_latents
+                        _prev_latents = _latents
+                        _latents = results[-1] * (1-self.traj_momentum) + self.traj_momentum * (1-self.traj_momentum) * _prev_latents + self.traj_momentum * self.traj_momentum * _prev_prev_latents if len(results)>0 else _latents
+                        _latents_n = additional_noise * self.ensemble_rate * (1 - self.momentum) ** (j+1) + _latents
+                    else:
+                        _latents_n = additional_noise * self.ensemble_rate + _latents
+                    if self.ensemble_guidance_scale:
+                        guidance_scale = guidance_scale
+                        rand = torch.randn(1).item()
+                    positive_guidance_scale = guidance_scale if not self.ensemble_guidance_scale else guidance_scale + rand
+                    negative_guidance_scale = 1.0 if not self.ensemble_guidance_scale else 1.0 + rand
+                    
                     _latent_model_input = torch.cat([_latents_n] * 2) if _do_classifier_free_guidance else _latents_n # Forward DDIM
                     _latent_model_input = _emergency_scheduler.scale_model_input(_latent_model_input, _lasting_t)
                     _noise_pred = self.unet(
@@ -150,7 +186,7 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                     ).sample
                     if _do_classifier_free_guidance:
                         _noise_pred_uncond, _noise_pred_text = _noise_pred.chunk(2)
-                        _noise_pred = _noise_pred_uncond + guidance_scale * (_noise_pred_text - _noise_pred_uncond)
+                        _noise_pred = _noise_pred_uncond + positive_guidance_scale * (_noise_pred_text - _noise_pred_uncond)
                     # Additional
                     _bsz, _channel, _frames, _width, _height = _latents_n.shape
                     _latents_n = _latents_n.permute(0, 2, 1, 3, 4).reshape(_bsz * _frames, _channel, _width, _height)
@@ -168,7 +204,7 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                     ).sample
                     if _do_classifier_free_guidance:
                         _noise_pred_uncond, _noise_pred_text = _noise_pred.chunk(2)
-                        _noise_pred = _noise_pred_uncond + guidance_scale * (_noise_pred_text - _noise_pred_uncond)
+                        _noise_pred = _noise_pred_uncond + negative_guidance_scale * (_noise_pred_text - _noise_pred_uncond)
                     _inv_extra_step_kwargs = copy.deepcopy(extra_step_kwargs)
                     _inv_extra_step_kwargs.pop("eta")
                     # Additional                  
@@ -178,7 +214,10 @@ class ModelScopeT2V_GN(TextToVideoSDPipeline):
                     _latents_n = _latents_n[None, :].reshape(_bsz, _frames, _channel, _width, _height).permute(0, 2, 1, 3, 4)
                     
                     results.append(_latents_n)
-                _latents = torch.stack(results,0).mean(0)
+                if self.fast_ensemble:
+                    _latents = results[-1]
+                else:
+                    _latents = torch.stack(results,0).mean(0)
                 
         print("Successfully Generate Optimium Noise")
         if kwargs.get("prompt_2", None) is not None:
